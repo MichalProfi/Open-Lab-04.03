46498ffd0c6e3d0be496aa1fdfaaf85dd61f2faa
Michal Holena <113421705+MichalProfi@users.noreply.github.com>
2022-10-27 08:45:04 +0200

    Removing first and last letter of string

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length > 2)
+                return original.Substring(1, original.Length - 2);
+            else if (original.Length > 0 && original.Length < 2)
+                return original;
+            else
+                return string.Empty;
         }
     }
 }
